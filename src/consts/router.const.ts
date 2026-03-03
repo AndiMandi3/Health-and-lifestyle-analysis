0a948800b5aca13d326a9df6c695a7b9b396e945
Димаков Андрей Владиславович <Dimakov_AV@sever.local>
2026-03-03 15:35:20 +0700

    git init

---
+++ src/consts/router.const.ts
@@ -0,0 +1,11 @@
+export const RouteName = {
+  MAIN_PAGE: "MainPage",
+  LOGIN_PAGE: "LoginPage",
+  REGISTER_PAGE: "RegisterPage",
+} as const;
+
+export const RouteTitle = {
+  MAIN_PAGE: "Главная страница",
+  LOGIN_PAGE: "Авторизация",
+  REGISTER_PAGE: "Регистрация",
+} as const;
